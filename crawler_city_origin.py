eeeaf6e7182c5cea95c8a95bd24177956ebc288c
xionghaoo <xionghao930316@gmail.com>
2018-09-12 16:25:42 +0800

    fix bug

--- crawler_city_origin.py
+++ crawler_city_origin.py
@@ -1,10 +1,8 @@
-# 抓取省、市数据
-
 import requests
 import json
 import codecs
 from bs4 import BeautifulSoup
-
+import re
 
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'}
 
@@ -34,7 +32,9 @@ for p in province_list:
     city_data = {}
     city_data['cities'] = []
     for c in city_list:
+
         city_html = c.contents
+        # td > a > text
         try:
             city_html_number = city_html[0].contents[0].contents[0]
         except AttributeError:
@@ -45,60 +45,20 @@ for p in province_list:
         except AttributeError:
             city_html_name = city_html[1].contents[0]
 
-        # 将市辖区改成直辖市名称
         city_obj = {}
-        if city_html_name == '市辖区':
-            city_obj['cityName'] = p.contents[0][0:-1]
-        else:
-            city_obj['cityName'] = city_html_name
+        city_obj['cityName'] = city_html_name
         city_obj['cityNumber'] = city_html_number
 
-        # 将重庆市的县对应的地区归整到重庆市市辖区
         city_data['provinceName'] = p.contents[0]
-        if p.contents[0] == '重庆市' and city_obj['cityName'] == '县':
-            break
-        else:
-            city_data['cities'].append(city_obj)
+        city_data['cities'].append(city_obj)
 
     print(city_data)
     data['provinces'].append(city_data)
 
-# 手动添加的台湾和香港地区数据，如果不需要可以注释掉
-taiwan = {}
-taiwan['provinceName'] = '台湾省'
-taiwan['cities'] = []
-t = {"cityName": "台北市", "cityNumber": "taiwan01"}
-taiwan['cities'].append(t)
-t = {"cityName": "新北市", "cityNumber": "taiwan02"}
-taiwan['cities'].append(t)
-t = {"cityName": "桃园市", "cityNumber": "taiwan03"}
-taiwan['cities'].append(t)
-t = {"cityName": "台中市", "cityNumber": "taiwan04"}
-taiwan['cities'].append(t)
-t = {"cityName": "台南市", "cityNumber": "taiwan05"}
-taiwan['cities'].append(t)
-t = {"cityName": "高雄市", "cityNumber": "taiwan06"}
-taiwan['cities'].append(t)
-data['provinces'].append(taiwan)
-
-hk = {}
-hk['provinceName'] = '香港'
-hk['cities'] = []
-t = {"cityName": "香港", "cityNumber": "hk01"}
-hk['cities'].append(t)
-data['provinces'].append(hk)
-
-aomen = {}
-aomen['provinceName'] = '澳门'
-aomen['cities'] = []
-t = {"cityName": "澳门", "cityNumber": "aomen01"}
-aomen['cities'].append(t)
-data['provinces'].append(aomen)
-
 # D:\\SVNRepo\\JZWealth\\app\\src\\main\\assets\\provinces.json
 with codecs.open('provinces.json', 'w') as f_obj:
     # 如果以utf8格式输出，ensure_ascii应该为True
-    json.dump(data, f_obj, ensure_ascii=True)
+    json.dump(data, f_obj, ensure_ascii=False)
 
 
 
--- crawler_country_origin.py
+++ crawler_country_origin.py
@@ -1,5 +1,3 @@
-# 抓取省、市、区数据
-
 import requests
 import json
 import codecs
@@ -52,25 +50,21 @@ for p in province_list:
             city_html_number = city_html[0].contents[0].contents[0]
         except AttributeError:
             city_html_number = city_html[0].contents[0]
-
         try:
-            tmp_city_html_name = city_html[1].contents[0].contents[0]
+            city_html_name = city_html[1].contents[0].contents[0]
             city_href = city_html[1].contents[0]['href']
         except AttributeError:
-            tmp_city_html_name = city_html[1].contents[0]
-
-        # 将市辖区改成直辖市名称
-        city_html_name = ''
-        if tmp_city_html_name == '市辖区':
-            city_html_name = p.contents[0][0:-1]
-        else:
-            city_html_name = tmp_city_html_name
+            city_html_name = city_html[1].contents[0]
         country_data = {}
         country_data['countries'] = []
+        # country_data['cityName'] = city_html_name
+        # country_data['cityNumber'] = city_html_number
 
         country_request = requests.get(href_host + city_href, headers=headers)
+        # country_request.encoding = 'gb2312'
         country_soup = BeautifulSoup(country_request.text.encode(country_request.encoding).decode('gbk'), 'html.parser')
         country_list = country_soup.select('tr.countytr')
+        # print(country_list)
         for country in country_list:
             country_data['cityName'] = city_html_name
             country_data['cityNumber'] = city_html_number
@@ -111,38 +105,7 @@ for p in province_list:
         city_data['cities'].append(country_data)
     data['provinces'].append(city_data)
 
-# 手动添加的台湾和香港地区数据，如果不需要可以注释掉
-taiwan = {}
-taiwan['provinceName'] = '台湾省'
-taiwan['cities'] = []
-t = {"cityName": "台北市", "cityNumber": "taiwan01"}
-taiwan['cities'].append(t)
-t = {"cityName": "新北市", "cityNumber": "taiwan02"}
-taiwan['cities'].append(t)
-t = {"cityName": "桃园市", "cityNumber": "taiwan03"}
-taiwan['cities'].append(t)
-t = {"cityName": "台中市", "cityNumber": "taiwan04"}
-taiwan['cities'].append(t)
-t = {"cityName": "台南市", "cityNumber": "taiwan05"}
-taiwan['cities'].append(t)
-t = {"cityName": "高雄市", "cityNumber": "taiwan06"}
-taiwan['cities'].append(t)
-data['provinces'].append(taiwan)
-
-hk = {}
-hk['provinceName'] = '香港'
-hk['cities'] = []
-t = {"cityName": "香港", "cityNumber": "hk01"}
-hk['cities'].append(t)
-data['provinces'].append(hk)
-
-aomen = {}
-aomen['provinceName'] = '澳门'
-aomen['cities'] = []
-t = {"cityName": "澳门", "cityNumber": "aomen01"}
-aomen['cities'].append(t)
-data['provinces'].append(aomen)
-
+# D:\\SVNRepo\\JZWealth\\app\\src\\main\\assets\\countries.json
 with codecs.open('countries.json', 'w') as f_obj:
     # 如果以utf8格式输出，ensure_ascii应该为True
     json.dump(data, f_obj, ensure_ascii=True)
